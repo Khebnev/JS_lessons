3bf98e4c60395d1f2f219374241004880332e034
Anton <khebnev.a.v@gmail.com>
2020-01-11 19:21:07 +0200

    Fixed for with typeof about not null

---
+++ Again/Lesson3/Theory/js/script.js
@@ -0,0 +1,49 @@
+//let str = "Test";
+//
+//console.log(str.toLowerCase());
+//console.log(str.toUpperCase());
+
+let str = "12.2px";
+
+//console.log(Math.round(str));
+console.log(parseFloat(str));
+console.log(parseInt(str));
+
+
+
+
+
+
+
+//let calc = (a,b) => a+b
+//
+//console.log(calc(4,5));
+//console.log(calc(4,15));
+//
+//let calc = function calc(a,b) {
+//  return   a+b;
+//}
+//console.log(calc(4,5));
+//console.log(calc(4,15));
+
+
+
+//let calc = function calc(a,b) {
+//  return   a+b;
+//}
+//console.log(calc(4,5));
+//console.log(calc(4,15));
+
+
+
+
+//    let num = 20;
+//
+//function showFirstMessage(text) {
+//    alert(text);
+//    num = 10;
+//}
+//
+//showFirstMessage("Hello, World");
+//console.log(num);
+
---
+++ Again/Lesson3/practice/js/script.js
@@ -0,0 +1,55 @@
+//Подзадача 2
+let money,
+    name,
+    time;
+
+function start() {
+    money = prompt("Ваш бюджет?", "");
+    name = prompt("Название Вашего магазина?", "");
+    time = 21;
+}
+
+start();
+//подзадача3
+let mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+}
+
+for (let i = 0; i < 5; i++) {
+
+    let a = prompt("Какой товар будем продавать?");
+
+    if ((typeof(a)) === 'string' &&  (typeof(a)) != null && a != '' && a.length < 50 ) {
+        console.log("Всё верно");
+        mainList.shopGoods[i] = a;
+        //теряем тип нашего товара. Чтобы этого избежать нехобходимо написать else с таким условием, чтобы вернуться к нашему вопросу заново.
+    } else {
+//        i = i - 1;
+    }
+}
+
+
+
+
+
+if(time < 0) {
+    console.log("Такого просто не может быть");
+} else if(time > 8 && time < 20) {
+    console.log("Время работать");
+           } else if(time < 24) {
+                   console.log("Уже слишком поздно!");
+                      } else {
+                          console.log("В сутках толь 24 часа!");
+                      }
+
+
+
+
+//подзадача5
+alert(mainList.budget / 30);
+
+console.log(mainList);
